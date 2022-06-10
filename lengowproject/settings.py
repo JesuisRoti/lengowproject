55dcfc8cd078d8b326f1412b92b971f4b641cc77
romain <romain.bouyx@with-madrid.com>
2022-06-10 18:58:05 +0200

    Yes

--- lengowproject/settings.py
+++ lengowproject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-8&!xcco!=b%mkq+6r%2#k-&qeuwb!+-+l-h1rw6vkw(%9_zu&="
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
